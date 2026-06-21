245b1e08f0b064ece7bd1d85d11d09e0abbf9aa1
VirusLox <viruslox@users.noreply.github.com>
2026-06-21 02:01:41 +0200

    Refactor BuildFilterComplex for improved media handling
    Updated the BuildFilterComplex function to enhance video and audio processing pipelines, including dynamic connections for hardware outputs.

--- internal/engine/mixer/mixer.go
+++ internal/engine/mixer/mixer.go
@@ -9,23 +9,52 @@ import (
 func BuildFilterComplex(cfg *models.Config) ([]string, string, string, string) {
 	var args []string
 
-	args = append(args, "videotestsrc", "pattern=black", "is-live=true", "!", "video/x-raw,width=1920,height=1080,framerate=30/1", "!", "comp.sink_0")
-	args = append(args, "compositor", "name=comp", "!")
-	args = append(args, "queue", "leaky=downstream", "max-size-buffers=1", "max-size-time=30000000", "!", "videoconvert", "!", "video/x-raw,format=RGBA", "!", "videoconvert", "!")
+	// 1. RAMO VIDEO: Dal compositor all'encoder, fino al nodo "vtee"
+	args = append(args,
+		"compositor", "name=comp", "background=black", "!",
+		"video/x-raw", "!", "videoconvert", "!",
+		"x264enc", "tune=zerolatency", "speed-preset=ultrafast", "!",
+		"h264parse", "!", "tee", "name=vtee",
+	)
 
-	args = append(args, "audiomixer", "name=acomp", "!")
+	// 2. RAMO AUDIO: Dall'audiomixer all'encoder AAC, fino al nodo "atee"
+	args = append(args,
+		"audiomixer", "name=acomp", "!",
+		"audioconvert", "!", "audioresample", "!",
+		"avenc_aac", "!", "aacparse", "!", "tee", "name=atee",
+	)
 
+	// 3. MASTER CLOCK: Battito continuo per prevenire il freeze di GStreamer
+	args = append(args,
+		"videotestsrc", "pattern=black", "is-live=true", "!",
+		"video/x-raw,width=1920,height=1080,framerate=30/1", "!", "comp.sink_0",
+	)
+
+	// 4. SORGENTI MULTIMEDIALI
 	if cfg.Input.ChromiumSource.Active {
-		args = append(args, "udpsrc", "port=50002", "caps=application/x-rtp,media=video,clock-rate=90000,encoding-name=VP8", "!", "rtpvp8depay", "!", "vp8dec", "!", "queue", "leaky=downstream", "max-size-buffers=1", "max-size-time=30000000", "!", "videoconvert", "!", "comp.sink_1")
-		args = append(args, "udpsrc", "port=50003", "caps=application/x-rtp,media=audio,clock-rate=48000,encoding-name=OPUS", "!", "rtpopusdepay", "!", "opusdec", "!", "queue", "leaky=downstream", "max-size-buffers=1", "max-size-time=30000000", "!", "audioconvert", "!", "audioresample", "!", "acomp.sink_1")
+		// WebRTC Video
+		args = append(args,
+			"udpsrc", "port=50002", "caps=application/x-rtp,media=video,clock-rate=90000,encoding-name=VP8", "!",
+			"rtpvp8depay", "!", "vp8dec", "!", "queue", "leaky=downstream", "max-size-buffers=1", "!",
+			"videoconvert", "!", "comp.sink_1",
+		)
+		// WebRTC Audio
+		args = append(args,
+			"udpsrc", "port=50003", "caps=application/x-rtp,media=audio,clock-rate=48000,encoding-name=OPUS", "!",
+			"rtpopusdepay", "!", "opusdec", "!", "queue", "leaky=downstream", "max-size-buffers=1", "!",
+			"audioconvert", "!", "audioresample", "!", "acomp.sink_1",
+		)
 	} else if cfg.Input.MediaSource.Active {
 		for i, layer := range cfg.Input.MediaSource.Layers {
 			if layer.Active {
-				args = append(args, "uridecodebin", "uri="+layer.InputPath, "name=src_"+fmt.Sprint(i))
-				args = append(args, "src_"+fmt.Sprint(i)+".", "!", "queue", "leaky=downstream", "max-size-buffers=1", "max-size-time=30000000", "!", "videoconvert", "!", fmt.Sprintf("comp.sink_%d", i+1))
-				args = append(args, "src_"+fmt.Sprint(i)+".", "!", "queue", "leaky=downstream", "max-size-buffers=1", "max-size-time=30000000", "!", "audioconvert", "!", "audioresample", "!", fmt.Sprintf("acomp.sink_%d", i+1))
+				srcName := fmt.Sprintf("src_%d", i)
+				args = append(args, "uridecodebin", "uri="+layer.InputPath, "name="+srcName)
+				// Collega dinamicamente le uscite hardware
+				args = append(args, srcName+".", "!", "queue", "leaky=downstream", "max-size-buffers=1", "!", "videoconvert", "!", fmt.Sprintf("comp.sink_%d", i+1))
+				args = append(args, srcName+".", "!", "queue", "leaky=downstream", "max-size-buffers=1", "!", "audioconvert", "!", "audioresample", "!", fmt.Sprintf("acomp.sink_%d", i+1))
 			}
 		}
 	}
+
 	return args, "", "", ""
 }
